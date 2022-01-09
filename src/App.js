6c92e31da5d91b56caf109219d8d16a6a5659078
Matt Lobo <mattl99@hotmail.ca>
2022-01-09 12:13:13 -0500

    submitting state lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    spiceLevel: 0,
+    spiceKeyword:"Nice"
+  };
+
+  changeSpice = (operator) => {
+    let newSpiceLevel = this.state.spiceLevel;
+    let newSpiceKeyword = this.state.spiceKeyword;
+    if (operator) {
+      newSpiceLevel += 1;
+    } else {
+      newSpiceLevel -= 1;
+    }
+    if (newSpiceLevel < 2){
+      newSpiceKeyword = "Nice"
+    }
+    if (newSpiceLevel >= 2 && newSpiceLevel < 5){
+      newSpiceKeyword = "Start Mopping Your Brow"
+    }
+    if (newSpiceLevel >= 5 && newSpiceLevel < 10){
+      newSpiceKeyword = "Watch Out"
+    }
+    if (newSpiceLevel >= 10 && newSpiceLevel < 15){
+      newSpiceKeyword = "Are you sure?"
+    }
+    if (newSpiceLevel >= 15 && newSpiceLevel < 20){
+      newSpiceKeyword = "All Guarantees are Void"
+    }
+    if (newSpiceLevel > 19){
+      newSpiceKeyword = "May Cause upset Stomach"
+    }
+    this.setState({ spiceLevel: newSpiceLevel, spiceKeyword: newSpiceKeyword });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>All Hail the Salad King </h1>
+        <button onClick={() => this.changeSpice("add")}>Add Some Heat</button>
+        <button onClick={() => this.changeSpice()}>Cool It Off</button>
+        <h2>Spice Level: {this.state.spiceLevel}</h2>
+        <h2>Status: {this.state.spiceKeyword} </h2>
+      </div>
+    );
+  }
 }
-
 export default App;
